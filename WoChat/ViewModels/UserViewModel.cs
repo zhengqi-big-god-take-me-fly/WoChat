b5af7eb9cc483168e491a92e14aa2ebac12201bb
perqin <perqinxie@gmail.com>
2016-05-12 00:25:06 +0800

    add UserViewModel

---
+++ WoChat/ViewModels/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoChat.Models;
+
+namespace WoChat.ViewModels {
+    /// <summary>
+    /// For the user information of this client
+    /// </summary>
+    class UserViewModel {
+        public UserModel LocalUser {
+            get {
+                return localUser;
+            }
+        }
+        private UserModel localUser;
+    }
+}
