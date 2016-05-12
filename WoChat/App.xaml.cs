52c24e22a001a5662729dbaa52da3983f6d91959
perqin <perqinxie@gmail.com>
2016-05-12 09:22:18 +0800

    Change home page

--- WoChat/App.xaml.cs
+++ WoChat/App.xaml.cs
@@ -57,7 +57,7 @@ namespace WoChat {
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(RegLogPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
